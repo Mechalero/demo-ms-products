14c22d97f7ac91c28b79c50e9af0c19fca3bfc1f
Chris Gaitan M <leider@if-cs.com>
2023-01-05 19:58:40 -0500

    add swagger for doc api & order in test

--- src/test/java/com/lcgm/ms/app/products/ProductRestControllerIntegrationTest.java
+++ src/test/java/com/lcgm/ms/app/products/ProductRestControllerIntegrationTest.java
@@ -7,7 +7,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +21,7 @@ import com.lcgm.ms.app.products.models.entity.Product;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProductRestControllerIntegrationTest {
 	
 
@@ -28,17 +32,7 @@ public class ProductRestControllerIntegrationTest {
 	private ObjectMapper objectMapper;
 
 	@Test
-	public void givenProduct_whenGetProduct_thenReturnJsonArray() throws Exception {
-		this.mockMvc.perform(get("/3")).andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.*").isNotEmpty());
-	}
-
-	@Test
-	public void givenProducts_whenGetProducts_thenReturnJsonArray() throws Exception {
-		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.*").isArray());
-	}
-
-	@Test
+	@Order(1)
 	public void givenProduct_whenCreateProduct_thenReturnIsCreate() throws Exception {
 		Product product = createProduct();
 		this.mockMvc.perform(post("/")
@@ -48,12 +42,25 @@ public class ProductRestControllerIntegrationTest {
 				.andExpect(status().isCreated())
 				.andExpect(jsonPath("$.*").isNotEmpty());
 	}
+	
+	@Test
+	@Order(2)
+	public void givenProduct_whenGetProduct_thenReturnJsonArray() throws Exception {
+		this.mockMvc.perform(get("/1")).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.*").isNotEmpty());
+	}
+	
+	@Test
+	@Order(3)
+	public void givenProducts_whenGetProducts_thenReturnJsonArray() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.*").isArray());
+	}
 
 	@Test
+	@Order(4)
 	public void givenProduct_whenDeleteProduct_thenReturnIsDelete() throws Exception {
-		this.mockMvc.perform(delete("/4")).andDo(print())
-				.andExpect(status().isNoContent())
-				.andExpect(jsonPath("$.*").isNotEmpty());
+		this.mockMvc.perform(delete("/1")).andDo(print())
+				.andExpect(status().isNoContent());
 	}
 
 	private Product createProduct() {
